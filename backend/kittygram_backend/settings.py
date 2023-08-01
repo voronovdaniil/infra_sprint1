865cf3c189e3b01ab194dad79afcd02bd57cf406
Daniil <d.a.voronov550@gmail.com>
2023-08-01 15:41:43 +0000

    Attempt to fix dotenv

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
-load_dotenv(find_dotenv())
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
