fcad7944675e1163a7f32f502c2c47118672e088
voronovdaniil <117637718+voronovdaniil@users.noreply.github.com>
2023-08-03 00:00:47 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = lover(os.getenv('DEBUG', 'False')) == 'True'
+DEBUG = lower(os.getenv('DEBUG', 'False')) == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split()
 
